416ff3ca9d43d44fe8755d7b0fc7378757dfba18
Alexey Piltenko <101993475+OleksiiPro1@users.noreply.github.com>
2023-01-10 18:00:19 +0100

    'Request failed with status code 400'

--- weatherApp/App.js
+++ weatherApp/App.js
@@ -1,20 +1,42 @@
+import axios from 'axios';
+import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = '36534d3dbf4da1a8bf1cabf1635529f3';
+
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&${longitude}&appid=${API_KEY}`,
+    );
+    console.log(data);
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const { coords: latitude, longitude } =
+        await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+
+      // console.log(coords.latitude, coords.longitude);
+    } catch (error) {
+      Alert.alert("Can't find you.", 'So sad');
+    }
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
